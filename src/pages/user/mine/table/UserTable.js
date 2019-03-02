e330a5a0c4ec0d08112a79f064975dbc1628b7b7
falstack <icesilt@outlook.com>
2019-03-02 20:43:39 +0800

    update: user page style

--- src/pages/user/mine/table/UserTable.js
+++ src/pages/user/mine/table/UserTable.js
@@ -26,11 +26,57 @@ export default class extends Component {
     return (
       <View className='user-panel'>
         <AtList hasBorder={false}>
-          <AtListItem title='我的主页' arrow='right' hasBorder={false} />
-          <AtListItem title='交易记录' arrow='right' hasBorder={false} />
-          <AtListItem title='我的邀请码' arrow='right' hasBorder={false} extraText='邀请送团子' />
-          <AtListItem title='帮助手册' arrow='right' hasBorder={false} />
-          <AtListItem title='意见反馈' arrow='right' hasBorder={false} />
+          <AtListItem
+            title='我的主页'
+            arrow='right'
+            hasBorder={false}
+            iconInfo={{
+              size: 20,
+              color: '#657786',
+              value: 'home'
+            }}
+          />
+          <AtListItem
+            title='交易记录'
+            arrow='right'
+            hasBorder={false}
+            iconInfo={{
+              size: 20,
+              color: '#657786',
+              value: 'shopping-bag'
+            }}
+          />
+          <AtListItem
+            title='我的邀请码'
+            arrow='right'
+            hasBorder={false}
+            extraText='邀请送团子'
+            iconInfo={{
+              size: 20,
+              color: '#657786',
+              value: 'sketch'
+            }}
+          />
+          <AtListItem
+            title='帮助手册'
+            arrow='right'
+            hasBorder={false}
+            iconInfo={{
+              size: 20,
+              color: '#657786',
+              value: 'help'
+            }}
+          />
+          <AtListItem
+            title='意见反馈'
+            arrow='right'
+            hasBorder={false}
+            iconInfo={{
+              size: 20,
+              color: '#657786',
+              value: 'phone'
+            }}
+          />
         </AtList>
       </View>
     )

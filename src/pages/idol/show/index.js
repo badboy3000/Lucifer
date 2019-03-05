20bb6e1a2e6a19731ff40113fbbee5b29863228a
falstack <icesilt@outlook.com>
2019-03-05 08:53:46 +0800

    add: component

--- src/pages/idol/show/index.js
+++ src/pages/idol/show/index.js
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
 import { AtTabs, AtTabsPane, AtList, AtListItem } from 'taro-ui'
 import http from '~/utils/http'
+import IdolInfo from './info/index'
 import './index.scss'
 
 export default class extends Component {
@@ -50,17 +51,6 @@ export default class extends Component {
     if (!idol) {
       return
     }
-    const hasLimited = idol.max_stock_count !== '0.00'
-    const hasBuyStock = idol.has_star !== '0.00'
-    const computedPercent = idol.has_star ? `持股占比：${parseFloat(
-      (idol.has_star / idol.star_count) * 100
-    ).toFixed(2)}%` : ''
-    const balance = idol.total_pay - idol.total_income
-    const productBalance = `￥${parseFloat(balance).toFixed(2)}`
-    const productPercent = balance ? `收益率：${parseFloat(
-      (balance / idol.market_price) * 100
-    ).toFixed(2)}%` : ''
-    const trending = idol.trending ? `市值排行：${idol.trending}` : ''
 
     return (
       <View>
@@ -77,17 +67,7 @@ export default class extends Component {
           ]}
           onClick={this.tabSwitch}>
           <AtTabsPane current={this.state.current} index={0}>
-            <AtList>
-              <AtListItem title='当前市值' extraText={idol.company_state ? `￥${idol.market_price}` : '未上市'} note={trending} />
-              <AtListItem title='每股价格' extraText={`￥${idol.stock_price}`} />
-              <AtListItem title='持股人数' extraText={`${idol.fans_count}人`} />
-              <AtListItem title='总发行股' extraText={hasLimited ? idol.max_stock_count : '无上限'} />
-              <AtListItem title='已认购股' extraText={idol.star_count} />
-              <AtListItem title='我持有股' extraText={hasBuyStock ? `${idol.has_star}` : '未入股'} note={computedPercent} />
-              <AtListItem title='公司支出' extraText={idol.company_state ? idol.total_pay : '-'} />
-              <AtListItem title='公司收入' extraText={idol.company_state ? idol.total_income : '-'} />
-              <AtListItem title='公司盈利' extraText={idol.company_state ? productBalance : '-'} note={productPercent} />
-            </AtList>
+            <IdolInfo idol={idol} />
           </AtTabsPane>
           <AtTabsPane current={this.state.current} index={1}>
             <View style='font-size:18px;text-align:center;height:100px;'>标签页二的内容</View>
---
+++ src/pages/idol/show/info/index.js
@@ -0,0 +1,41 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import { AtTabsPane, AtList, AtListItem } from 'taro-ui'
+import './index.scss'
+
+export default class extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    const { idol } = this.props
+    const hasLimited = idol.max_stock_count !== '0.00'
+    const hasBuyStock = idol.has_star !== '0.00'
+    const computedPercent = hasBuyStock ? `持股占比：${parseFloat(
+      (idol.has_star / idol.star_count) * 100
+    ).toFixed(2)}%` : ''
+    const balance = idol.total_pay - idol.total_income
+    const productBalance = `￥${parseFloat(balance).toFixed(2)}`
+    const productPercent = balance ? `收益率：${parseFloat(
+      (balance / idol.market_price) * 100
+    ).toFixed(2)}%` : ''
+    const trending = idol.trending ? `市值排行：${idol.trending}` : ''
+
+    return (
+      <AtTabsPane current={this.state.current} index={0}>
+        <AtList>
+          <AtListItem title='当前市值' extraText={idol.company_state ? `￥${idol.market_price}` : '未上市'} note={trending} />
+          <AtListItem title='每股价格' extraText={`￥${idol.stock_price}`} />
+          <AtListItem title='持股人数' extraText={`${idol.fans_count}人`} />
+          <AtListItem title='总发行股' extraText={hasLimited ? idol.max_stock_count : '无上限'} />
+          <AtListItem title='已认购股' extraText={idol.star_count} />
+          <AtListItem title='我持有股' extraText={hasBuyStock ? `${idol.has_star}` : '未入股'} note={computedPercent} />
+          <AtListItem title='公司支出' extraText={idol.company_state ? `￥${idol.total_pay}` : '-'} />
+          <AtListItem title='公司收入' extraText={idol.company_state ? `￥${idol.total_income}` : '-'} />
+          <AtListItem title='公司盈利' extraText={idol.company_state ? productBalance : '-'} note={productPercent} />
+        </AtList>
+      </AtTabsPane>
+    )
+  }
+}
